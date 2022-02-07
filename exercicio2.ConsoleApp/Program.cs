c56a98091146745a4b2a97e8230d6f40df878674
Tatiane Mossi <mossitatiane2@gmail.com>
2022-02-07 19:57:17 -0300

    Implementando o segundo exercício da lista de exercícios 2.

--- exercicio2.ConsoleApp/Program.cs
+++ exercicio2.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace exercicio2.ConsoleApp
 {
@@ -10,25 +11,30 @@ namespace exercicio2.ConsoleApp
         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite um número");
+            Console.WriteLine("Digite o primeiro número:");
             int a = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Digite um número");
+            Console.WriteLine("Digite o segundo número:");
             int b = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Digite um número");
+            Console.WriteLine("Digite o terceiro número:");
             int c = Convert.ToInt32(Console.ReadLine());
 
             if (a == b || b == c || a == c)
-                Console.WriteLine(" Os números não são diferentes");
+                Console.WriteLine("Os números não são diferentes");
             else
-                Console.WriteLine("");
+            {
+                int[] listaValoresEntrada = { a, b, c };
+                listaValoresEntrada = listaValoresEntrada.OrderByDescending(c => c).ToArray();
 
+                Console.WriteLine("A lista de números em ordem decrescente é: ");
+
+                foreach (int value in listaValoresEntrada)
+                {
+                    Console.Write(value + " ");
+                }
+            }                        
             Console.ReadLine();
-            
-            
-                
-                 
         }
     }
 }
