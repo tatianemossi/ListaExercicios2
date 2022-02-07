64fc95c16c5591888c92c152eae039837efba15d
Tatiane Mossi <mossitatiane2@gmail.com>
2022-02-07 19:24:50 -0300

    Add project files.

---
+++ exercicio1.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicio1.ConsoleApp
+{
+    internal class Program
+    {
+
+        /*
+         * Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C. 
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor para A:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor para B:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor para C:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            
+            if (a + b >= c)
+                Console.WriteLine("A soma de a + b não é menor que c");
+            else 
+                Console.WriteLine("A soma de a + b é menor que c");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exercicio2.ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercicio2.ConsoleApp
+{
+    internal class Program
+    {
+
+        /*
+         * Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem decrescente.
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um número");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um número");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a == b || b == c || a == c)
+                Console.WriteLine(" Os números não são diferentes");
+            else
+                Console.WriteLine("");
+
+            Console.ReadLine();
+            
+            
+                
+                 
+        }
+    }
+}
