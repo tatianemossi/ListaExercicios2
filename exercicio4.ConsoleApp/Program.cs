e0b74dd715964afc79aad424b212bdb82debb11d
Tatiane Mossi <mossitatiane2@gmail.com>
2022-02-07 20:44:39 -0300

    Implementando o quarto exercício da lista de exercícios 2.

---
+++ exercicio4.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicio4.ConsoleApp
+{
+    internal class Program
+    {
+        //Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if ((numero % 2) == 0)
+                Console.WriteLine("O número que você digitou é par");
+            else
+                Console.WriteLine("O número que você digitou é ímpar");
+
+            Console.ReadLine();
+        }
+    }
+}
